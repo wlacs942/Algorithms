a48cc8e198fa8e2edd6fca65272a0977370064f6
mohamed Shalabi <mohammed_gamal1985@yahoo.com>
2017-12-16 07:39:44 -0800

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,3 +1,13 @@
+//1.     Enter some integers, and press “enter” after each one.
+
+//2.     Entering the integer 0 will end the process of entering the integers
+
+//3.     The program will then print out the first remaining member of the queue one at a time.
+
+//4.     It will then print the number of integers remaining it the queue
+
+//5.     The 0 will not be printed, nor counted as one of the remaining integers.
+
 #include <iostream> // library for manipulating output
 #include <queue>
 
