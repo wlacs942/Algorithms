c6548443b974bdb28a07c3485984174a89c25214
ja172 <aguil172@gmail.com>
2017-12-17 19:15:29 -0800

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -13,23 +13,23 @@ bool search(int value, int values[], int m);
 
 int main()
 {
-//Declorations
-int nums[] = {41, 39, 43, 40, 52, 63, 88, 32, 54, 66, 12, 16, 5, 8}; //array of integers(int size must equal the amount of elements)
-int size = 14; //Size of array (needs to be adjusted whenever the number of elements in array nums[] is changed)
+	//Declorations
+	int nums[] = {41, 39, 43, 40, 52, 63, 88, 32, 54, 66, 12, 16, 5, 8}; //array of integers(int size must equal the amount of elements)
+	int size = 14; //Size of array (needs to be adjusted whenever the number of elements in array nums[] is changed)
 	
-//Starts sort
-sort(nums, size);
+	//Starts sort
+	sort(nums, size);
 
-//Prints Sorted Array of nums[]
-for (int i = 0; i < size; i++)
-{
-if (i < (size - 1))
-cout << nums[i] << " \n";
-} cout << endl;
+	//Prints Sorted Array of nums[]
+	for (int i = 0; i < size; i++)
+	{
+		if (i < (size - 1))
+			cout << nums[i] << " \n";
+	} cout << endl;
 
-//Return Success
-system("pause");
-return 0;
+	//Return Success
+	system("pause");
+	return 0;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -38,25 +38,25 @@ return 0;
 //Function to sort
 void sort(int values[], int s)
 {
-//Temp Declorations
-int counter; //used to determine loop restart
-int holder;  //value holder when when swapping arrays elements
+	//Temp Declorations
+	int counter; //used to determine loop restart
+	int holder;  //value holder when when swapping arrays elements
 
-do //Sort
-{
-//initialize counter here to allow loop entrance
-counter = 0; //resets to 0 when condition fails to exit loop
-
-//start sort loop
-for (int i = 0; i < (s - 1); i++)
-{
-if (values[i] > values[i + 1])
-{                               
-holder = values[i];         //////////
-values[i] = values[i + 1];  //Swapping
-values[i + 1] = holder;     //////////
-counter++;                  //prevents loop exit if two elements are swapped
-}
-}
-} while (counter != 0); //condition required to exit loop
+	do //Sort
+	{
+	//initialize counter here to allow loop entrance
+	counter = 0; //resets to 0 when condition fails to exit loop
+		
+		//start sort loop
+		for (int i = 0; i < (s - 1); i++)
+		{
+		if (values[i] > values[i + 1])
+		{                               
+		holder = values[i];         //////////
+		values[i] = values[i + 1];  //Swapping
+		values[i + 1] = holder;     //////////
+		counter++;                  //prevents loop exit if two elements are swapped
+		}
+		}
+	} while (counter != 0); //condition required to exit loop
 }
